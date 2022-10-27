8e6089f4301b360cbd528c3916d14875b576d7e7
NASA_Mark <76894588+Atharane@users.noreply.github.com>
2022-10-27 21:50:38 +0530

    add 404 page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Login from "./Components/Login/Login.tsx";
+import Login from "./Components/Login/Login.tsx"; 
+import PageNotFound from "./Components/PageNotFound.tsx"; 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login/>} />
+        <Route path="*" element={<PageNotFound/>} />
       </Routes>
     </BrowserRouter>
   );
